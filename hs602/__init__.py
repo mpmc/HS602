8294fda3a395ca5758bb77c18bfe2a9d515fdef4
mpmc <mpmc@users.noreply.github.com>
2019-05-05 22:03:33 +0100

    Whitespace fixes
    Signed-off-by: mpmc <mpmc@users.noreply.github.com>

--- hs602/__init__.py
+++ hs602/__init__.py
@@ -1,19 +1,19 @@
 # Copyright (C) 2019 Mark Clarkstone <mpmc@disroot.org>
-# 
+#
 # This file is part of hs602.
-# 
+#
 # hs602 is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # hs602 is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with hs602.  If not, see <http://www.gnu.org/licenses/>.
 """HS602 capture utilities."""
 # Version
-_VERSION_ = "0.3.4.dev1"
+__version__ = __VERSION__ = "0.3.4.dev1"
